bbc3fb66e94bcf1004932b0c46a24c8dd78832e8
bloorq@gmail.com <bloorq@gmail.com>
2019-02-07 00:58:40 +0000

    fix errors in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-import pathlib
 from setuptools import setup
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
     name="ctfpwn",
     version="1.0.0",
     description="Framework for making CTFs, bug bounty and pentesting Python scripting easier",
-    long_description=README,
+    long_description="README.md",
     long_description_content_type="text/markdown",
     url="https://github.com/bl4de/ctfpwn",
     author="Rafal Janicki @bl4de",
@@ -25,7 +18,7 @@ setup(
     ],
     packages=["ctfpwn"],
     include_package_data=True,
-    install_requires=["requests", "hashlib", "base64"],
+    install_requires=["requests", "hashlib"],
     entry_points={
         "console_scripts": [
             "ctfpwn=ctfpwn.__main__:main",
