6d449ccb2328dcc84219802d3d3d0695226d140c
bloorq@gmail.com <bloorq@gmail.com>
2019-02-07 02:04:41 +0000

    net; http_get() function

--- ctfpwn/net.py
+++ ctfpwn/net.py
@@ -1,3 +1,10 @@
+import requests
 
-def foo():
-    print "foo() from ctfpwn.net"
+
+def http_get(url):
+    """
+    executes regular HTTP GET request to url
+    returns response body
+    """
+    resp = requests.get(url)
+    return resp.content
