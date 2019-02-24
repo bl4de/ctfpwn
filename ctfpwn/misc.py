34748074dded5396a5b56e109bf0fc1a30df7322
bloorq@gmail.com <bloorq@gmail.com>
2019-02-24 10:48:19 +0000

    misc.py - transform_string_into_array().

--- ctfpwn/misc.py
+++ ctfpwn/misc.py
@@ -4,7 +4,7 @@
 def split_string(__string, __chunk_length):
     """
     splits string into __chunk_length len chunks
-    
+
     >>> import ctfpwn
     >>> s = "57702A6C58744751386538716E6D4D59552A737646486B6A49742A52"
     >>> print ctfpwn.split_string(s, 2)
@@ -20,3 +20,20 @@ def split_string(__string, __chunk_length):
         str_pos = str_pos + __chunk_length
 
     return chunked
+
+
+def transform_string_into_array(__string, __array_type):
+    """
+    transforms strigs into array of predefined values for further processing
+    handled __array_type values:
+    'binary' - transforms into array of 8-bit chunks like ['10011001',...]
+    'ascii' - transforms into array of ASCII codes like ['56', '2B', ...]
+    """
+    separator = ' '
+    if __array_type == 'binary':
+        return split_string(__string, 8).split(separator)
+    if __array_type == 'ascii':
+        return split_string(__string, 2).split(separator)
+
+    # return __string if no __array_type is defined or unknown
+    return __string
